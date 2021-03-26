0fe21c3dbc5c24e745e4c674e36eff1a6f037a00
Laura DiSanza <ldisanza7@gmail.com>
2021-03-26 14:46:31 -0500

    JS Added

--- js/script.js
+++ js/script.js
@@ -1,122 +1,43 @@
-var noon = 12;
-var evening = 18; // 6PM
-var wakeupTime = 9; // 9AM
-var lunchTime = 12; // 12PM
-var partyTime = 17; // 5PM
-var napTime = lunchTime + 2; // 2PM
-var time = new Date().getHours();
-var partyTimeButton = document.getElementById("partyTimeButton");
-var napTimeSelector = document.getElementById("napTimeSelector");
-var lunchTimeSelector = document.getElementById("lunchTimeSelector");
-var wakeUpTimeSelector = document.getElementById("wakeUpTimeSelector");
-var isPartyTime = false;
+/* Error message when field is blank */
+document.getElementById("trial-form").onsubmit = function () {
+     var w = document.forms["trial-form"]["first"].value;
+     var x = document.forms["trial-form"]["last"].value;
+     var y = document.forms["trial-form"]["email"].value;
+     var z = document.forms["trial-form"]["password"].value;
+ 
+     var submit = true;
 
- 
-var updateClock = function(){
- 
-     var lolcat = document.getElementById('lolcat');
-     var message = document.getElementById('timeEvent');
-     var messageText;
-     var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
- 
-     if (time == partyTime){
-          image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
-          messageText = "IZ PARTEE TIME!!";
-     } else if (time == napTime) {
-          image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/napTime.jpg";
-          messageText = "IZ NAP TIME...";
-     } else if (time == lunchTime) {
-          image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/lunchTime.jpg";
-          messageText = "IZ NOM NOM NOM TIME!!";
-     } else if (time == wakeupTime) {
-          image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/wakeUpTime.jpg";
-          messageText = "IZ TIME TO GETTUP.";
-     } else if (time < noon) {
-          messageText = "Good morning!";
-     } else if (time > evening) {
-          messageText = "Good Evening!";
-     }else {
-          messageText = "Good afternoon!";
+     if (w == null || w == "") {
+         firstError = "Please enter your first name";
+         document.getElementById("first_error").innerHTML = firstError;
+         submit = false;
+
+     if (x == null || x == "") {
+         lastError = "Please enter your last name";
+         document.getElementById("last_error").innerHTML = lastError;
+         submit = false;
      }
  
-     message.innerText = messageText;
-     lolcat.src = image;
- 
-     showCurrentTime();
- 
-};
- 
-var showCurrentTime = function() {
-     // display the string on the webpage
-     var clock = document.getElementById('clock');
-     
-     var currentTime = new Date();
-     var hours = currentTime.getHours();
-     var minutes = currentTime.getMinutes();
-     var seconds = currentTime.getSeconds();
-     var meridian = "AM";
- 
-     // Set Hours
-     if (hours >= noon){
-          meridian = "PM";
+     if (y == null || y == "") {
+         emailError = "Please enter your email";
+         document.getElementById("email_error").innerHTML = emailError;
+         submit = false;
      }
  
-     if (hours > noon){
-          hours = hours - 12;
+     if (z == null || z == "") {
+         passwordError = "Please enter your password";
+         document.getElementById("password_error").innerHTML = passwordError;
+         submit = false;
      }
  
-     // Set Minutes
-     if (minutes < 10){
-          minutes = "0" + minutes;
-     }
+     return submit;
+ }
  
-     // Set Seconds
-     if (seconds < 10){
-          seconds = "0" + seconds;
-     }
+ function removeWarning() {
+     document.getElementById(this.id + "_error").innerHTML = "";
+ }
  
-     // put together the string that displays the time
-     var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian + "!";
- 
-     clock.innerText = clockTime;
-};
- 
-updateClock();
- 
-var oneSecond = 1000;
- 
-setInterval( updateClock, oneSecond);
-
-// Party time event
-var partyEvent = function() {
- 
-   if (isPartyTime === false) {
-      isPartyTime = true;
-      time = partyTime;
-      partyTimeButton.innerText = "PARTY TIME!";
-      partyTimeButton.style.backgroundColor = "#222";
-   } else {
-      isPartyTime = false;
-      time = new Date().getHours();
-      partyTimeButton.innerText = "PARTY OVER";
-      partyTimeButton.style.backgroundColor = "#0A8DAB";
-   }
-};
-
-partyTimeButton.addEventListener('click', partyEvent);
-
- var lunchEvent = function() {
-    lunchTime = lunchTimeSelector.value;
-};
- 
-var wakeUpEvent = function() {
-    wakeupTime = wakeUpTimeSelector.value;
-};
- 
-var napEvent = function() {
-    napTime = napTimeSelector.value;
-};
-
-napTimeSelector.addEventListener('change', napEvent);
-lunchTimeSelector.addEventListener('change', lunchEvent);
-wakeUpTimeSelector.addEventListener('change', wakeUpEvent);
+ document.getElementById("first").onkeyup = removeWarning;
+ document.getElementById("last").onkeyup = removeWarning;
+ document.getElementById("email").onkeyup = removeWarning;
+ document.getElementById("password").onkeyup = removeWarning;
